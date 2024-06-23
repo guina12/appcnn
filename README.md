0666bc56723f0c78a20342e27de4dc91bc4706f8
GuinaDataScience <115325442+guina12@users.noreply.github.com>
2024-06-22 23:32:11 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# CnnDetection
+## Detection of Drowsiness Using Convolutional Neural Networks (CNNs)
+
+### Software And Tools Requirements
+
+1.[GitHubAccount](https://github.com)
+2.[VsCodeIDE](https://code.visualstudio.com/)
+3.[HerokuAccount](https://heroku.com)
+4.[GitCLI](https://git-scm.com/downloads)
+
+
+Create a new invironment
+```
+python -m venv myenv
+
+```
+
---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from PIL import Image
+import numpy as np
+
+# Carregar o modelo ao iniciar o aplicativo
+@st.cache_resource
+def load_my_model():
+    return load_model('/content/model_image.h5')
+
+# Pré-processar a imagem
+def preprocess_image(image):
+    image = image.resize((244, 244))  # Redimensiona a imagem
+    image = np.array(image)  # Converte para array numpy
+    image = preprocess_input(image)  # Pré-processa a imagem para ResNet50
+    image = np.expand_dims(image, axis=0)  # Adiciona dimensão de lote
+    return image
+
+def main():
+    st.title("Classificação de Imagens com CNN")
+    st.write("Carregue uma imagem e o modelo CNN fará a previsão.")
+
+    uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Imagem Carregada', use_column_width=True)
+        st.write("")
+        st.write("Classificando...")
+
+        model = load_my_model()
+        processed_image = preprocess_image(image)
+        prediction = model.predict(processed_image)
+
+        if prediction >= 0.5:
+            st.write("A previsão é: Probabilidade {:.2f} olhos fechados ".format(float(prediction)))
+        else:
+            st.write("A previsão é: Probabilidade {:.2f}  olhos abertos".format(float(prediction)))
+
+if __name__ == "__main__":
+    main()
